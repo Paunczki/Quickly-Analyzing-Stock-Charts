d4100cef904a95414bd875a877f549b4198bf557
= <epautsch@luc.edu>
2022-04-21 04:30:42 -0500

    Array creation enabled through ArrayFromPic class

---
+++ ArrayFromPic.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+# A class to generate and hold a binary-valued array from an input picture.
+# Construction requires a Pillow Image object as an argument.
+# Call ArrayFromPic.mapped_array to get array.
+
+
+class ArrayFromPic:
+
+    def __init__(self, im):
+
+        width = im.size[0]
+        height = im.size[1]
+        data = list(im.getdata(0))
+
+        self.mapped_array = np.zeros((width, height))
+        index_count = 0
+        row = 0
+        column = 0
+        for x in data:
+            if x > 1:
+                self.mapped_array[row][column] = 1
+            index_count += 1
+            column += 1
+            if index_count % width == 0:
+                row += 1
+                column = 0
+
--- main.py
+++ main.py
@@ -1,116 +1,33 @@
 from PIL import Image
-from Maze import Maze
-import time
+from ArrayFromPic import ArrayFromPic as afp
 import argparse
 
 
-# A simple factory class that imports and returns a relevant solver when provided a string
-# Not hugely necessary, but reduces the code in solve.py, making it easier to read.
-
-class SolverFactory:
-    def __init__(self):
-        self.Default = "breadthfirst"
-        self.Choices = ["breadthfirst","depthfirst","dijkstra", "astar","leftturn"]
-
-    # def createsolver(self, type):
-    #     if type == "leftturn":
-    #         import leftturn
-    #         return ["Left turn only", leftturn.solve]
-    #     elif type == "depthfirst":
-    #         import depthfirst
-    #         return ["Depth first search", depthfirst.solve]
-    #     elif type == "dijkstra":
-    #         import dijkstra
-    #         return ["Dijkstra's Algorithm", dijkstra.solve]
-    #     elif type == "astar":
-    #         import astar
-    #         return ["A-star Search", astar.solve]
-    #     else:
-    #         import breadthfirst
-    #         return ["Breadth first search", breadthfirst.solve]
-
-
-def solve(factory, method, input_file, output_file):
+def solve(input_file, output_file):
     # Load Image
-    print ("Loading Image")
+    print("Loading Image")
     im = Image.open(input_file)
+    array_from_pic = afp(im)
+    for row in array_from_pic.mapped_array:
+        print(row)
+    print(im)
 
-    # Create the maze (and time it) - for many mazes this is more time consuming than solving the maze
-    print ("Creating Maze")
-    t0 = time.time()
-    maze = Maze(im)
-    t1 = time.time()
-    print ("Node Count:", maze.count)
-    total = t1-t0
-    print ("Time elapsed:", total, "\n")
-
-    # # Create and run solver
-    # [title, solver] = factory.createsolver(method)
-    # print ("Starting Solve:", title)
-    #
-    # t0 = time.time()
-    # [result, stats] = solver(maze)
-    # t1 = time.time()
-
-    stats = [0,0,0,0,0]
-
-    total = t1-t0
-
-    # Print solve stats
-    print ("Nodes explored: ", stats[0])
-    if (stats[2]):
-        print ("Path found, length", stats[1])
-    else:
-        print ("No Path Found")
-    print ("Time elapsed: ", total, "\n")
-
-    """
-    Create and save the output image.
-    This is simple drawing code that travels between each node in turn, drawing either
-    a horizontal or vertical line as required. Line colour is roughly interpolated between
-    blue and red depending on how far down the path this section is.
-    """
-
-    print ("Saving Image")
+    print("Saving Image")
     im = im.convert('RGB')
-    impixels = im.load()
-
-    # resultpath = [n.Position for n in result]
-    #
-    # length = len(resultpath)
-
-    # for i in range(0, length - 1):
-    #     a = resultpath[i]
-    #     b = resultpath[i+1]
-    #
-    #     # Blue - red
-    #     r = int((i / length) * 255)
-    #     px = (r, 0, 255 - r)
-    #
-    #     if a[0] == b[0]:
-    #         # Ys equal - horizontal line
-    #         for x in range(min(a[1],b[1]), max(a[1],b[1])):
-    #             impixels[x,a[0]] = px
-    #     elif a[1] == b[1]:
-    #         # Xs equal - vertical line
-    #         for y in range(min(a[0],b[0]), max(a[0],b[0]) + 1):
-    #             impixels[a[1],y] = px
 
     im.save(output_file)
 
 
+# Built in Python argparse used to grab string names from console. To use, uncomment the commented section and comment
+# out the hardcoded solve function
 def main():
-    sf = SolverFactory()
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--method", nargs='?', const=sf.Default, default=sf.Default,
-                        choices=sf.Choices)
-    parser.add_argument("input_file")
-    parser.add_argument("output_file")
-    args = parser.parse_args()
-    solve(sf, args.method, args.input_file, args.output_file)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("input_file")
+    # parser.add_argument("output_file")
+    # args = parser.parse_args()
+    # # solve(args.input_file, args.output_file)
+    solve('graphs/Graph13.jpg', 'output13.jpg')  # comment this out if using argparse
 
 
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
