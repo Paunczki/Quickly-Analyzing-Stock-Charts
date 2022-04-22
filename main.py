efde0ca4f01d36b4216b0252bf37a05d1024356d
John Mikos <30276245+Paunczki@users.noreply.github.com>
2022-04-21 21:17:57 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from PIL import Image
+from ArrayFromPic import ArrayFromPic as afp
+import argparse
+
+
+def solve(input_file, output_file):
+    # Load Image
+    print("Loading Image")
+    im = Image.open(input_file)
+    array_from_pic = afp(im)
+    for row in array_from_pic.mapped_array:
+        print(row)
+    print(im)
+
+    print("Saving Image")
+    im = im.convert('RGB')
+
+    im.save(output_file)
+
+
+# Built in Python argparse used to grab string names from console. To use, uncomment the commented section and comment
+# out the hardcoded solve function
+def main():
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("input_file")
+    # parser.add_argument("output_file")
+    # args = parser.parse_args()
+    # # solve(args.input_file, args.output_file)
+    solve('graphs/Graph13.jpg', 'output13.jpg')  # comment this out if using argparse
+
+
+if __name__ == '__main__':
+    main()
